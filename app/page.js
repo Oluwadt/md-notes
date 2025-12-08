6eda9ebada495b397668e070624a889c472e9f43
Oluwadt <dtonilede@gmail.com>
2025-12-08 14:10:01 +0100

    created Login component

--- app/page.js
+++ app/page.js
@@ -1,9 +1,9 @@
-import Image from "next/image";
+import Login from "./components/Login";
 
 export default function Home() {
   return (
-    <div>
-      Hello World
-    </div>
+    <main>
+      <Login />
+    </main>
   );
 }
